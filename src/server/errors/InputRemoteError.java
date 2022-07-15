2411f59655a4b0df1cd31f96032f73eb692bdb1e
Anteneh Kassaw Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-07-15 15:03:33 -0700

    Input error remote exception

---
+++ src/server/errors/InputRemoteError.java
@@ -0,0 +1,11 @@
+package server.errors;
+
+import utils.Color;
+import java.rmi.RemoteException;
+
+public class InputRemoteError extends RemoteException {
+
+    public InputRemoteError(){
+        super(Color.RED + "[ Invalid input, please input a positive number up to two decimal places and try again! ]" + Color.RESET);
+    }
+}
