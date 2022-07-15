b5e1bb7ed3b198f4f758bf635e009cb94453a817
Anteneh Kassaw Bizuneh <88293613+Anteneh2121@users.noreply.github.com>
2022-07-15 13:16:50 -0700

    Functional login

---
+++ scripts/compile.sh
@@ -0,0 +1,8 @@
+rm -rf "..out/"
+mkdir -p "../out/classes/client"
+mkdir -p "../out/classes/server"
+
+javac ../src/server/*.java ../src/server/errors/*.java ../src/client/*.java ../src/client/errors/*.java ../src/utils/*.java -d ../out/classes/
+echo "--------------------------------------------------"
+echo "Finished compiling"
+echo "--------------------------------------------------"
