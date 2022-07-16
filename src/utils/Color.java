706736651da011f4e19035cf4f9a5d326c2d2474
Tesfay Daniel <58104091+TesfayDaniel@users.noreply.github.com>
2022-07-16 09:30:22 +0300

    adding colors

---
+++ src/utils/Color.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
+    public static final String BBLACK = "\u001b[30;1m";
+    public static final String BRED = "\u001b[31;1m";
+    public static final String BGREEN = "\u001b[32;1m";
+    public static final String BYELLOW = "\u001b[33;1m";
+    public static final String BBLUE = "\u001b[34;1m";
+    public static final String BMAGENTA = "\u001b[35;1m";
+    public static final String BCYAN = "\u001b[36;1m";
+    public static final String BWHITE = "\u001b[37;1m";
+}
